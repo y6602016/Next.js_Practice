6330298001e80de9d9b3f966dd5e9282329d6136
mike <y6602016@gmail.com>
2021-07-16 16:31:52 +0800

    update text

--- pages/about.js
+++ pages/about.js
@@ -9,18 +9,8 @@ const About = () => {
       </Head>
       <div>
         <h1>About</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book
-        </p>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book
-        </p>
+        <p>Lorem Ipsum is simply dummy text</p>
+        <p>Lorem Ipsum is simply dummy text</p>
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,8 @@ export default function Home() {
       </Head>
       <div>
         <h1 className={styles.title}>Home</h1>
-        <p className={styles.text}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book
-        </p>
-        <p className={styles.text}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book
-        </p>
+        <p className={styles.text}>Lorem Ipsum is simply dummy text</p>
+        <p className={styles.text}>Lorem Ipsum is simply dummy text</p>
         <Link href="/ninjas">
           <a className={styles.btn}>See Ninja Listing</a>
         </Link>
